767d91785d9e222b9fc2bd8bbcdedf6e27e91905
Lody <lodyyusuf22@gmail.com>
2025-05-20 22:19:54 +0300

    adding address, gender, and age fields to users table creating UserController editing to  in User model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,20 +46,13 @@ class UserController extends Controller
             ],401);
         }
         $validated = $request->validate([
-            'name' => 'nullable|string|max:255',
-            'email' => 'nullable|email|unique:users,email,' . $user->id,
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $user->id,
             'address' => 'nullable|string',
             'gender' => 'nullable|in:male,female,other',
-            'age' => 'nullable|integer|min:0',
+            'age' => 'nullable|integer|min:1',
             'password' => 'nullable|string|min:6',
         ]);
-
-        if ($validated->fails()) {
-            return response()->json([
-                'Message'=>'Profile Not Changed!',
-                'Errors'=> $validated->errors()
-            ],422);
-        }
 
         if (isset($validated['password'])) {
             $validated['password'] = Hash::make($validated['password']);

4fe31562c6f344db586acccbc58c181f0b959094
Obaida Almelhem <almelhemo@gmail.com>
2025-05-22 21:13:29 +0300

    update apis order to prevent overlapping

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,24 +18,23 @@ Route::middleware('auth:api')->group(function () {
 Route::middleware("auth:api")->prefix('categories')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);
     Route::post('/', [CategoryController::class, 'store']);
+    Route::get('/hierarchy', [CategoryController::class, 'hierarchy']);
     Route::get('/{id}', [CategoryController::class, 'show']);
     Route::put('/{id}', [CategoryController::class, 'update']);
     Route::delete('/{id}', [CategoryController::class, 'destroy']);
     Route::get('/{id}/subcategories', [CategoryController::class, 'getWithSubcategories']);
     Route::get('/{id}/products', [CategoryController::class, 'getWithProducts']);
-    Route::get('/hierarchy/all', [CategoryController::class, 'hierarchy']);
 });
 
 // Product Routes
 Route::middleware("auth:api")->prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
     Route::post('/', [ProductController::class, 'store']);
+    Route::get('/expired', [ProductController::class, 'getExpired']);
+    Route::get('/expiring-soon', [ProductController::class, 'getExpiringSoon']);
+    Route::post('/search', [ProductController::class, 'search']);
+    Route::post('/stocks/update', [ProductController::class, 'updateStocks']);
     Route::get('/{id}', [ProductController::class, 'show']);
     Route::put('/{id}', [ProductController::class, 'update']);
     Route::delete('/{id}', [ProductController::class, 'destroy']);
-    Route::get('/{id}/category', [ProductController::class, 'getWithCategory']);
-    Route::get('/expired/all', [ProductController::class, 'getExpired']);
-    Route::get('/expiring-soon/all', [ProductController::class, 'getExpiringSoon']);
-    Route::post('/search', [ProductController::class, 'search']);
-    Route::post('/stocks/update', [ProductController::class, 'updateStocks']);
 });

f6bedcf865594081c54505afd005115d0a78d93f
talalcodes55 <talalalghazal55@gmail.com>
2025-05-25 20:30:46 +0300

    created a profile page

---
+++ frontend/web/app/profile/page.tsx
@@ -0,0 +1,82 @@
+"use client"
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { useUser } from '@/contexts/UserContext'
+import { User } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+
+export default function Profile() {
+    const { user, loadUser} = useUser();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!user) {
+            loadUser();
+        }
+    }, [user, loadUser]);
+
+    const handleLogout = async () => {
+        const res = await fetch("/api/logout", {
+            method: "POST",
+        });
+
+
+        if (res.ok) {
+            console.log("USER AFTER LOGGING OUT: " + user);
+            router.replace("/");
+        }
+    };
+
+
+    return (
+        <>
+            <div className="flex flex-col font-[family-name:var(--font-geist-sans)] p-8">
+                <h2 className='text-2xl font-bold'>Profile</h2>
+                <div className=" w-100 md:w-150 h-full p-8 rounded-2xl">
+                    <div className="flex items-center bg-gray-100 px-8 py-5 rounded-2xl space-x-5 mb-7">
+                        <Avatar className='w-[50px] h-[50px]'>
+                            <AvatarFallback className='border border-black'>
+                                <User />
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col">
+                            <div className="flex space-x-2">
+                                <h1 className='text-lg'>{user?.name}</h1>
+                                <Badge variant={'outline'}>Admin</Badge>
+                            </div>
+                            <h3 className='text-muted-foreground text-sm'>{user?.email}</h3>
+                        </div>
+                    </div>
+                    <div className="grid sm:grid-cols-2 gap-5">
+                        <div className="">
+                            <h1>User ID</h1>
+                            <div className="p-3 bg-gray-100 rounded-xl">
+                                {user?.id}
+                            </div>
+                        </div>
+                        {/* Some hardcoded stuff: */}
+                        <div className="">
+                            <h1>Age</h1>
+                            <div className="p-3 bg-gray-100 rounded-xl">
+                                25
+                            </div>
+                        </div>
+                        <div className="">
+                            <h1>Gender</h1>
+                            <div className="p-3 bg-gray-100 rounded-xl">
+                                Male
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex justify-between mt-7'>
+                        <Button className='' onClick={handleLogout} variant={'destructive'}>Logout</Button>
+                        <Button className='' variant={'default'}>Edit profile</Button>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}

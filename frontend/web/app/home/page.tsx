42675a0ba057b30b686d409ca746bd82c3ee9746
talalcodes55 <talalalghazal55@gmail.com>
2025-05-25 20:30:35 +0300

    removed user info from the home page

--- frontend/web/app/home/page.tsx
+++ frontend/web/app/home/page.tsx
@@ -1,35 +1,17 @@
 "use client"
-
-import { Button } from '@/components/ui/button';
 import { useUser } from '@/contexts/UserContext';
-import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 export default function Homepage() {
 
-    const { user, loading, loadUser } = useUser();
+    const { user, loadUser } = useUser();
     useEffect(() => {
         if (!user) {
             loadUser();
         }
     }, [user, loadUser]);
-
-    const handleLogout = async () => {
-        const res = await fetch("/api/logout", {
-            method: "POST",
-        });
-
-        if (res.ok) {
-            console.log("USER AFTER LOGGING OUT: " + user);
-            redirect("/");
-        }
-    };
-
-    if (loading) return <p>Loading...</p>;
     return (
         <>
-            <div className="flex flex-col font-[family-name:var(--font-geist-sans)] p-8">
-                <h2 className='text-3xl font-bold'>Profile</h2>
-
-            </div>
+            <h1>this is the home page - Doha and Layal are creating this page </h1>
         </>
     );
 }

4ba33203dafbc542ddbc262361c0416802678b1a
talalcodes55 <talalalghazal55@gmail.com>
2025-05-26 19:49:51 +0300

    created the profile edit page

---
+++ frontend/web/app/profile-edit/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileEditForm from '@/components/ProfileEditForm';
+
+export default function ProfileEdit() {
+
+
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <ProfileEditForm></ProfileEditForm>
+        </div>
+    )
+}

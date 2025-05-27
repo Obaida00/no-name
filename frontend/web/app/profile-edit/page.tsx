925b160ab19978e0d8b2de2eef4e0def77528934
talalcodes55 <talalalghazal55@gmail.com>
2025-05-27 20:09:23 +0300

    added responsiveness to the profile edit page

--- frontend/web/app/profile-edit/page.tsx
+++ frontend/web/app/profile-edit/page.tsx
@@ -5,8 +5,12 @@ export default function ProfileEdit() {
 
 
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <ProfileEditForm></ProfileEditForm>
+        <div className="flex flex-col justify-center w-full not-lg:items-center min-h-screen  p-10 ">
+            <h1 className='text-2xl mb-10 font-[family-name:var(--font-geist-sans)]'>Edit your info</h1>
+            <div className="">
+                <ProfileEditForm></ProfileEditForm>
+            </div>
+
         </div>
     )
 }
--- frontend/web/components/ProfileEditForm.tsx
+++ frontend/web/components/ProfileEditForm.tsx
@@ -8,6 +8,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { useForm } from 'react-hook-form';
 import { Input } from './ui/input';
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '@/components/ui/select';
+import { Button } from './ui/button';
 export default function ProfileEditForm() {
     const { user } = useUser();
     const ProfileSchema = z.object({
@@ -15,6 +16,7 @@ export default function ProfileEditForm() {
         age: z.number().min(20, { message: "Your age must not be less than 20 years" }),
         gender: z.string()
     });
+
 
     const form = useForm<z.infer<typeof ProfileSchema>>({
         resolver: zodResolver(ProfileSchema),
@@ -25,46 +27,50 @@ export default function ProfileEditForm() {
         }
     });
     return (
-        <div className="min-h-full">
-            <h1>form</h1>
-            <div className="grid">
-                <Form {...form}>
-                    <form action="">
+        <div className="flex justify-between font-[family-name:var(--font-geist-sans)] not-md:p-5">
+            <Form {...form}>
+                <form action="" className=' md:w-full flex lg:flex-row flex-col justify-center lg:justify-between md:items-end h-100'>
+                    <div className='md:w-[450px] border border-dashed rounded-2xl md:self-start p-8'>
                         <FormField control={form.control} name='name' render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Name</FormLabel>
-                                <FormControl><Input {...field} type="text" /></FormControl>
+                                <FormControl><Input {...field} className='mb-4' type="text" /></FormControl>
                                 <FormMessage />
                             </FormItem>
                         )} />
-                        <FormField control={form.control} name='age' rules={{ min: 20 }} render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Age</FormLabel>
-                                <FormControl><Input {...field} type="number" /></FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )} />
-                        <FormField control={form.control} name='gender' render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Age</FormLabel>
-                                <FormControl><Select {...field} onValueChange={field.onChange} defaultValue={field.value}>
-                                    <SelectTrigger className="w-[180px]">
-                                        <SelectValue placeholder="Select a fruit" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectLabel>Gender</SelectLabel>
-                                            <SelectItem value="male">Male</SelectItem>
-                                            <SelectItem value="female">Female</SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select></FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )} />
-                    </form>
-                </Form>
-            </div>
+                        <div className="flex not-md:flex-col items-start">
+                            <FormField control={form.control} name='age' rules={{ min: 20 }} render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Age</FormLabel>
+                                    <FormControl><Input {...field} className='mb-4' type="number" min={20} /></FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                            <FormField control={form.control} name='gender' render={({ field }) => (
+                                <FormItem className='md:ml-2 w-full'>
+                                    <FormLabel>Gender</FormLabel>
+                                    <FormControl><Select {...field} onValueChange={field.onChange} defaultValue={field.value}>
+                                        <SelectTrigger className='w-full'>
+                                            <SelectValue />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Gender</SelectLabel>
+                                                <SelectItem value="male">Male</SelectItem>
+                                                <SelectItem value="female">Female</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select></FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                        </div>
+                    </div>
+                    <div>
+                        <Button variant={'default'} className='mt-7 not-lg:w-full'>Save changes</Button>
+                    </div>
+                </form>
+            </Form>
         </div>
     )
 }

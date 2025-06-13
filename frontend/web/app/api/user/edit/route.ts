2b2290e3f685f51e73a2588bdd23cef540880b18
talalcodes55 <talalalghazal55@gmail.com>
2025-06-14 01:06:41 +0300

    fixed the profile edit api route and added proper url

--- frontend/web/app/api/user/edit/route.ts
+++ frontend/web/app/api/user/edit/route.ts
@@ -1,29 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function PUT(request: NextRequest) {
     try {
         const token = request.cookies.get("token")?.value;
         if (!token) {
             return NextResponse.json({"message": "Unauthorized"}, {"status": 200});
         }
-        const requestBody = request.json();
-        const response = await fetch("REPLACE THIS WITH THE REAL REQUEST URL WHEN IT IS DONE", {
-            method: "POST",
+        const requestBody = await request.json();
+        const response = await fetch(`${process.env.LARAVEL_API_BASE_URL}/api/users/${requestBody.id}`, {
+            method: "PUT",
             body: JSON.stringify(
                 requestBody
             ),
             headers: {
-                "Content-Type": "application/json",
                 "Accept": "application/json",
-                "Authorization" : `Bearer ${token}`
+                "Authorization" : `Bearer ${token}`,
+                "Content-Type": "application/json",
             },
         });
 
-        if (response.ok) {
+if (response.ok) {
             console.log("From Next Server: User is edited with success");
-            return NextResponse.json({data: response.json(), status: 200});
+            const data = await response.json();
+            return NextResponse.json({data: data, status: 200});
         }
     } catch (error) {
         return NextResponse.json({message: "From Next Server: Error Processing request", error})
     }
-}
+}        

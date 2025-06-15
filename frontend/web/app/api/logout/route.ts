bf16824e6959802642ccbeaa64f6fc5862c76b47
talalcodes55 <talalalghazal55@gmail.com>
2025-06-16 01:47:23 +0300

    created a Laravel API logout request in the logout api route

--- frontend/web/app/api/logout/route.ts
+++ frontend/web/app/api/logout/route.ts
@@ -1,13 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
-  const response = NextResponse.json(
-    { message: "Logged out successfully" },
-    { status: 200 }
-  );
+export async function POST(request: NextRequest) {
+  try {
+    const token = request.cookies.get("token")?.value;
+    const response = await fetch(
+      `${process.env.LARAVEL_API_BASE_URL}/api/logout`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-
-  response.cookies.delete("token")
-
-  return response;
+    if (!response.ok) {
+      return NextResponse.json({ message: "Could not log out now, try again" });
+    }
+    request.cookies.delete("token");
+    return NextResponse.json(
+      { message: "Logged out successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }

ec4ed33b69c7bd1917a02898bb3e7eb91ba587fe
talalcodes55 <talalalghazal55@gmail.com>
2025-06-16 01:48:21 +0300

    created the function that sends profile edit request to the Laravel API from the user route

--- frontend/web/app/api/user/route.ts
+++ frontend/web/app/api/user/route.ts
@@ -10,6 +10,7 @@ export async function GET(request: NextRequest) {
     const laravelRes = await fetch(
       `${process.env.LARAVEL_API_BASE_URL}/api/user`,
       {
+        method: "GET",
         headers: {
           Accept: "application/json",
           Authorization: `Bearer ${token}`,
@@ -18,7 +19,7 @@ export async function GET(request: NextRequest) {
     );
 
     const { data: user } = await laravelRes.json();
-
+    
     if (!laravelRes.ok) {
       return NextResponse.json(
         { message: "Laravel error" },
@@ -26,11 +27,41 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    console.log("User fetched from Laravel:", user);
-
     return NextResponse.json(user);
   } catch (error) {
     console.error("Server error:", error);
     return NextResponse.json({ message: "Server error" }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const token = request.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const requestBody = await request.json();    
+    const response = await fetch(
+      `${process.env.LARAVEL_API_BASE_URL}/api/user`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(requestBody),
+      }
+    );
+    if (!response.ok) {
+      return NextResponse.json("error");
+    }
+      const data = await response.json();
+      return NextResponse.json({ user: data.data, status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      message: "From Next Server: Error Processing request",
+      error,
+    });
   }
 }
